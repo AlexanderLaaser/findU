b25284cf723a58d2881f2d828c1612a30775f650
Alexander Laaser <alexlaaser@hotmail.de>
2023-10-31 17:05:19 +0100

    added reducer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Headernew from "./components-header/AppHeader.tsx";
 import { Box, Button, createTheme, Modal, ThemeProvider } from "@mui/material";
 import GridLayout from "./components-body/GridLayout.tsx";
+import { useReducer } from "react";
 
 const theme = createTheme({
   typography: {
